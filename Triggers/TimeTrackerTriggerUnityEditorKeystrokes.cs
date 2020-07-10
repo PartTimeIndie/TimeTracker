10bdc63e5635b7193b16a20aaade7fbcaca08e6c
PartTimeIndie <68103455+PartTimeIndie@users.noreply.github.com>
2020-07-10 20:45:22 +0200

    Create TimeTrackerTriggerUnityEditorKeystrokes.cs

---
+++ Triggers/TimeTrackerTriggerUnityEditorKeystrokes.cs
@@ -0,0 +1,40 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityBase
+{
+    [InitializeOnLoad]
+    public class TimeTrackerTriggerUnityEditorKeystrokes
+    {
+        private static DateTime lastTime =DateTime.MinValue;
+        static TimeTrackerTriggerUnityEditorKeystrokes()
+        {
+            System.Reflection.FieldInfo info = typeof(EditorApplication).GetField("globalEventHandler", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            EditorApplication.CallbackFunction value = (EditorApplication.CallbackFunction)info.GetValue(null);
+
+            value += () =>
+            {
+                if (Event.current.type == EventType.KeyDown)
+                {
+                    //we only need to trigger it every 10 seconds
+                    //doing to many triggers will just cause to many writes to disk without any need
+                    if ((DateTime.Now - lastTime).TotalSeconds > 10)
+                    {
+                        lastTime = DateTime.Now;
+                        TimeTracker.OnTrigger();
+                    }
+                }
+            };
+
+            info.SetValue(null, value);
+        }
+    }
+}
+#endif
